7f6e277341a679eb7ec886825e8dea95f4ef2de3
François Zuliani <zulianifrancois@yahoo.fr>
2024-09-12 19:04:48 +0200

    finish with navbar, create login component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
   selectedOption: string = '';
 
   selectOption(option: string) {
-    this.selectedOption = option;
-    console.log('Option selected:', this.selectedOption);
+    this.router.navigate([`/${option}`]);
   }
 }
